518e4d74a770ba357451d91c0a6884c6e66f5a20
artem.korotenko <artem.korotenko@betterme.world>
2021-06-07 23:58:22 +0300

    Added basic event tracking

--- Runtime/Core/General/UnibricsAttribute.cs
+++ Runtime/Core/General/UnibricsAttribute.cs
@@ -1,10 +1,12 @@
 namespace Unibrics.Core
 {
     using System;
+    using JetBrains.Annotations;
 
     /// <summary>
     /// Base class for all framework-specific attributes
     /// </summary>
+    [MeansImplicitUse]
     public class UnibricsAttribute : Attribute
     {
         
