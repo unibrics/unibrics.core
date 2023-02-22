a4e4006c2467cc148c3405bf9ca582f25e5cba9d
artem.korotenko <artem.korotenko@betterme.world>
2023-02-22 22:32:38 +0200

    added standalone components

---
+++ Runtime/Core/DI/Standalones/InstallStandaloneObjectsCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace Unibrics.Core.Standalone
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using DI;
+    using Execution;
+
+    public class InstallStandaloneObjectsCommand : ExecutableCommand
+    {
+        [Inject]
+        public IAttributedInstancesFactory<StandaloneAttribute, object> Factory { get; set; }
+
+        private List<object> standaloneComponents;
+
+        protected override void ExecuteInternal()
+        {
+            standaloneComponents = Factory.GetInstances().ToList();
+        }
+    }
+}
---
+++ Runtime/Core/DI/Standalones/StandaloneAttribute.cs
@@ -0,0 +1,11 @@
+namespace Unibrics.Core.Standalone
+{
+    using System;
+    using JetBrains.Annotations;
+
+    [AttributeUsage(AttributeTargets.Class), MeansImplicitUse]
+    public class StandaloneAttribute : Attribute
+    {
+        
+    }
+}
